5afb6e7540b16edbb1b58515115504d7761da7fe
Dacio Romero <dacioromero@gmail.com>
2018-11-28 16:47:40 -0800

    Add current progress

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -20,9 +20,17 @@ it("should say hello", function() {
 // Level 1 Challenges
 // ========================================================
 
-it("should return the area of a 5 by 6 rectangle");
+it("should return the area of a 5 by 6 rectangle", function() {
+  const area = utils.area(5, 6);
+  expect(area).to.be.a("number");
+  expect(area).to.equal(30);
+});
 
-it("should return the are of a circle of radius 5");
+it("should return the area of a circle of radius 5", function() {
+  const circleArea = utils.circleArea(5);
+  expect(circleArea).to.be.a("number");
+  expect(circleArea).to.be.closeTo(78.5398, 0.0001)
+});
 
 // ========================================================
 // Level 2 Challenges
@@ -32,15 +40,66 @@ it("should return the are of a circle of radius 5");
 // tests eventually.
 // ========================================================
 
-it("Should create a new (object) Item with name and price");
+it("Should create a new (object) Item with name and price", function() {
+  const banana = utils.Item('Banana', 1.02);
+  expect(banana).to.be.a('object');
+  expect(banana).to.have.property('name', 'Banana');
+  expect(banana).to.have.property('price', 1.02);
+});
 
-it("Should return an array containing all items in cart");
+const generateCart = () => {
+  const Item = utils.Item;
+  return utils.Cart(Item('Banana', 1.02), Item('Apple', 1.42), Item('Orange', 1.36));
+}
 
-it("Should add a new item to the shopping cart");
+it("Should return an array containing all items in cart", function() {
+  const cart = generateCart();
+  expect(cart).to.be.an('array');
+  expect(cart).to.deep.equal([
+    {
+      name: 'Banana', 
+      price: 1.02
+    },
+    {
+      name: 'Apple', 
+      price: 1.42
+    },
+    {
+      name: 'Orange', 
+      price: 1.36
+    }
+  ]);
+});
 
-it("Should return the number of items in the cart");
+it("Should add a new item to the shopping cart", function() {
+  const cart = generateCart();
+  cart.add(utils.Item('Romaine', 0.43))
+  expect(cart).to.deep.contain({
+    name: 'Romaine',
+    price: 0.43
+  },);
+});
 
-it("Should remove items from cart");
+it("Should return the number of items in the cart", function() {
+  const cart = generateCart();
+  expect(cart.count).to.equal(3)
+});
+
+it("Should remove items from cart", function() {
+  const cart = generateCart();
+  cart.remove(0);
+  expect(cart).with.lengthOf(2);
+  expect(cart).to.deep.equal([
+    {
+      name: 'Apple', 
+      price: 1.42
+    },
+    {
+      name: 'Orange', 
+      price: 1.36
+    }
+  ])
+});
 
 // ========================================================
 // Stretch Challenges
@@ -50,4 +109,7 @@ it("Should update the count of items in the cart");
 
 it("Should remove an item when count is 0");
 
-it("Should return the total cost of all items in the cart");
+it("Should return the total cost of all items in the cart", function() {
+  const cart = generateCart();
+  expect(cart.price).to.equal(3.80)
+});
--- utils.js
+++ utils.js
@@ -14,4 +14,30 @@ const circleArea = r => {
   return Math.PI * r * r;
 };
 
-module.exports = { sayHello, area, perimeter, circleArea };
+const Item = (name, price) => {
+  return {name, price}
+}
+
+const Cart = (...items) => {
+  items.add = function(item) {
+    this.push(item)
+  }
+  items.remove = function(index) {
+    this.splice(index, 1)
+  }
+  Object.defineProperties(items, {
+    price: {
+      get: function() {
+        return this.reduce((total, item) => total + item.price, 0)
+      }
+    },
+    count: {
+      get: function() {
+        return this.length;
+      }
+    }
+  })
+  return items
+}
+
+module.exports = { sayHello, area, perimeter, circleArea, Item, Cart };
